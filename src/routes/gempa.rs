78ffe7a5caaad48745ae0b580edb0cb7d923b671
yapie <targaryen.akane@gmail.com>
2021-03-03 01:59:33 +0700

    update bmkg-wrapper

--- src/routes/gempa.rs
+++ src/routes/gempa.rs
@@ -1,34 +1,29 @@
-use bmkg_wrapper::gempa;
-use gempa::Url::*;
+use bmkg_wrapper::gempa::{self, Url};
+use rocket::http::Status;
 use rocket_contrib::json::Json;
 use serde_json::{json, Value};
 use std::error::Error;
 
 #[get("/gempa/<data>")]
-pub fn gempa_data(data: String) -> Result<Json<Value>, Box<dyn Error>> {
-  let url = match data.as_str() {
-    "autogempa" => Autogempa,
-    "gempaterkini" => GempaTerkini,
-    "gempadirasakan" => GempaDirasakan,
-    "lasttsunami" => LastTsunami,
-    "en_autogempa" => EnAutogempa,
-    "en_gempaterkini" => EnGempaTerkini,
-    _ => Autogempa,
-  };
-  let data = gempa::get_data(url).and_then(gempa::to_json);
-
-  match data {
-    Ok(val) => Ok(Json(json!({ "data": val }))),
-    Err(e) => Err(e),
-  }
+pub fn gempa_data(data: String) -> Result<Json<Value>, Status> {
+    match Url::from_str(data) {
+        Some(url) => {
+            let data = gempa::get_data(url).and_then(gempa::to_json);
+            match data {
+                Ok(val) => Ok(Json(json!({ "data": val }))),
+                _ => Err(Status::InternalServerError),
+            }
+        }
+        None => Err(Status::NotFound),
+    }
 }
 
 #[get("/gempa")]
 pub fn gempa() -> Result<Json<Value>, Box<dyn Error>> {
-  let data = gempa::get_data(Autogempa).and_then(gempa::to_json);
+    let data = gempa::get_data(Url::Autogempa).and_then(gempa::to_json);
 
-  match data {
-    Ok(val) => Ok(Json(json!({ "data": val }))),
-    Err(e) => Err(e),
-  }
+    match data {
+        Ok(val) => Ok(Json(json!({ "data": val }))),
+        Err(e) => Err(e),
+    }
 }
