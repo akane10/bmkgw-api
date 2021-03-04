45cff6d064adfa87f552e04af57b2ef495efaf7b
yapie <targaryen.akane@gmail.com>
2021-03-04 07:52:14 +0700

    update

--- src/routes/gempa.rs
+++ src/routes/gempa.rs
@@ -1,18 +1,18 @@
-use bmkg_wrapper::gempa::{self, Url};
+use bmkg_wrapper::gempa::{self, Gempa, Url};
 use bmkg_wrapper::Error;
 use rocket::http::Status;
 use rocket_contrib::json::Json;
-use serde_json::{json, Value};
+// use serde_json::{json, Value};
 use tokio;
 
 #[get("/gempa/<data>")]
 #[tokio::main]
-pub async fn gempa_data(data: String) -> Result<Json<Value>, Status> {
+pub async fn gempa_data(data: String) -> Result<Json<Vec<Gempa>>, Status> {
     match Url::from_str(data) {
         Some(url) => {
             let data = gempa::get_data(url).await;
             match data {
-                Ok(val) => Ok(Json(json!({ "data": val }))),
+                Ok(val) => Ok(Json(val)),
                 _ => Err(Status::InternalServerError),
             }
         }
@@ -22,7 +22,7 @@ pub async fn gempa_data(data: String) -> Result<Json<Value>, Status> {
 
 #[get("/gempa")]
 #[tokio::main]
-pub async fn gempa() -> Result<Json<Value>, Error> {
+pub async fn gempa() -> Result<Json<Vec<Gempa>>, Error> {
     let data = gempa::get_data(Url::Autogempa).await?;
-    Ok(Json(json!({ "data": data })))
+    Ok(Json(data))
 }
